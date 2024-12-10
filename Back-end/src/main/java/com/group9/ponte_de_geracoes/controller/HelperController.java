dca90f37a91ea61692a4f939361cee5361e45687
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-10 20:08:49 -0300

    #14 - Implementa requisição PUT no controlador de Helper

--- Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/HelperController.java
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/HelperController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ import com.group9.ponte_de_geracoes.model.Helper;
 import com.group9.ponte_de_geracoes.repository.HelperRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/helper")
@@ -50,6 +53,33 @@ public class HelperController {
         URI locator = createNewURIById(insertedHelper);
 
         return ResponseEntity.created(locator).body(insertedHelper);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Helper> updateHelper(@PathVariable Long id, @Validated @RequestBody Helper requestHelper) {
+        return helperRepository.findById(id).map(helperToUpdate -> {
+            if (requestHelper.getName() != null) {
+                helperToUpdate.setName(requestHelper.getName());
+            }
+            if (requestHelper.getAge() != null){
+                helperToUpdate.setAge(requestHelper.getAge());
+            }
+            if (requestHelper.getEmail() != null){
+                helperToUpdate.setEmail(requestHelper.getEmail());
+            }
+            if (requestHelper.getPhone() != null){
+                helperToUpdate.setPhone(requestHelper.getPhone());
+            }
+            if (requestHelper.getAddress() != null){
+                helperToUpdate.setAddress(requestHelper.getAddress());
+            }
+            if (requestHelper.getSkills() != null){
+                helperToUpdate.setSkills(requestHelper.getSkills());
+            }
+
+            helperRepository.save(helperToUpdate);
+            return ResponseEntity.ok(helperToUpdate);
+        }).orElse(ResponseEntity.notFound().build());
     }
     
 }
