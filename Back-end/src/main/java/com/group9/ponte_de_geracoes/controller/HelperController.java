3d40dca0dc7022380f5757796e55fa4f5ed5cb0a
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-10 20:07:25 -0300

    #14 - Implementa requisição POST no controlador de Helper

--- Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/HelperController.java
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/HelperController.java
@@ -1,5 +1,7 @@
 package com.group9.ponte_de_geracoes.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.group9.ponte_de_geracoes.model.Helper;
 import com.group9.ponte_de_geracoes.repository.HelperRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/helper")
@@ -18,6 +23,16 @@ public class HelperController {
 
     @Autowired
     private HelperRepository helperRepository;
+
+    private URI createNewURIById(Helper helper){
+        URI locator = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(helper.getId())
+                    .toUri();
+        
+        return locator;
+    }
     
     @GetMapping
     public ResponseEntity<Page<Helper>> getHelpers(@PageableDefault(size = 10, sort = {"id"}) Pageable pageable){
@@ -26,4 +41,15 @@ public class HelperController {
         return ResponseEntity.ok(page);
     }
 
+    @PostMapping
+    public ResponseEntity<Helper> insertNewHelpers(@RequestBody Helper helper) { 
+        helper.setId(null);
+
+        Helper insertedHelper = helperRepository.save(helper);
+
+        URI locator = createNewURIById(insertedHelper);
+
+        return ResponseEntity.created(locator).body(insertedHelper);
+    }
+    
 }
