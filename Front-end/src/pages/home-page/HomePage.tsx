b3b4d19e33f2a4b8dc33982b94d07df8d05fb85b
vpaesi <paesivitoria@gmail.com>
2024-12-10 16:44:40 -0300

    #4 - Implementação completa da Home Page

--- Front-end/src/pages/home-page/HomePage.tsx
+++ Front-end/src/pages/home-page/HomePage.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import "./HomePage.css";
-import headerImg from "../../assets/header-img.jpg";
-import helper1 from "../../assets/helper1.jpg";
-import helper2 from "../../assets/helper2.jpg";
-import helper3 from "../../assets/helper3.jpg";
-import helper4 from "../../assets/helper4.jpg";
-import helper5 from "../../assets/helper5.jpg";
-import helper6 from "../../assets/helper6.jpg";
+import { Images } from "../../assets/Images";
 import { Link } from "react-router-dom";
 import BenefitCard from "../../components/benefit-card/BenefitCard";
 import Carousel from "../../components/carousel/Carousel";
@@ -17,27 +11,32 @@ const App: React.FC = () => {
     {
       title: "Facilidade no Dia a Dia",
       description: "Recebem ajuda em tarefas que podem ser desafiadoras, como fazer compras, buscar medicamentos ou resolver questões burocráticas.",
-      icon: undefined,
+      icon: Images.iconCalendar,
     },
     {
       title: "Saúde e Bem-Estar",
       description: "Recebem suporte para manter uma rotina mais saudável, como ajuda em atividades físicas, incentivo para consultas médicas regulares e orientações para cuidados pessoais.",
+      icon: Images.iconHeart,
     },
     {
       title: "Maior Autonomia",
       description: "Continuam vivendo de forma independente em suas casas, com o suporte necessário para as atividades diárias.",
+      icon: Images.iconFreedom,
     },
     {
       title: "Companhia e Socialização",
       description: "O contato com os ajudantes pode reduzir a solidão e proporcionar conversas e trocas de experiências.",
+      icon: Images.iconSocial,
     },
     {
       title: "Segurança",
       description: "Contam com uma rede confiável de apoio, o que traz tranquilidade para lidar com suas necessidades.",
+      icon: Images.iconShield,
     },
     {
       title: "Valorização",
       description: "Sentem-se parte da comunidade, com suas histórias e sabedoria reconhecidas e valorizadas.",
+      icon: Images.iconTrophy,
     },
   ];
 
@@ -45,64 +44,70 @@ const App: React.FC = () => {
     {
       title: "Desenvolvimento Pessoal",
       description: "Aprendem com as experiências e histórias dos idosos, ganhando uma nova perspectiva sobre a vida.",
+      icon: Images.iconStar,
     },
     {
       title: "Fortalecimento de Valores",
       description: "Desenvolvem empatia, paciência e responsabilidade ao ajudar quem precisa.",
+      icon: Images.iconShield,
     },
     {
       title: "Reconhecimento Social",
       description: "Sentem-se valorizados ao fazer parte de um projeto que promove impacto positivo na sociedade.",
+      icon: Images.iconTrophy
     },
     {
-      title: "Aprimoramento de Habilidades",
+      title: "Novas Habilidades",
       description: "Adquirem habilidades práticas, como comunicação e resolução de problemas, que podem ser úteis em outras áreas da vida.",
+      icon: Images.iconBooks
     },
     {
-      title: "Ampliação de Redes de Contatos",
+      title: "Redes de Contatos",
       description: "Conhecem novas pessoas, fortalecendo laços e criando conexões com benefícios pessoais e profissionais.",
+      icon: Images.iconSocial,
     },
     {
       title: "Sentimento de Propósito",
       description: "Participam de uma causa significativa, contribuindo para a construção de uma comunidade mais solidária.",
+      icon: Images.iconCompass
     },
   ];
 
   const registered = [
     {
       name: "Mariana",
-      yo: 24,
-      img: helper1,
+      age: 24,
+      img: Images.helper1,
       description: "Estudante de enfermagem apaixonada por cuidar de pessoas. Adoro ouvir histórias e ajudar em pequenas tarefas do dia a dia.",
     },
     {
       name: "Lucas",
-      yo: 30,
-      img: helper2,
+      age: 30,
+      img: Images.helper2,
       description: "Engenheiro que acredita no poder da empatia. Disponível para ajudar em compras, organização e resolver pendências.",
     },
     {
       name: "Sofia",
-      yo: 21,
-      img: helper3,
+      age: 21,
+      img: Images.helper3,
       description: "Universitária com paixão por leitura e tecnologia. Amo compartilhar meu tempo ajudando e aprendendo com os mais experientes.",
     },
     {
       name: "Gabriel",
-      yo: 28,
-      img: helper4,
+      age: 28,
+      img: Images.helper4,
       description: "Professor de educação física, gosta de incentivar a prática de exercícios e ajudar na organização de rotinas saudáveis.",
     },
     {
       name: "Ana",
-      yo: 26,
-      img: helper5,
+      age: 26,
+      img: Images.helper5,
       description: "Advogada que se dedica ao voluntariado. Adoro ajudar em questões burocráticas e compartilhar conhecimento.",
     },
     {
       name: "Ricardo",
-      yo: 35,
-      img: helper6,
+      age: 35,
+      img: Images.helper6,
       description: "Chef de cozinha apaixonado por ensinar receitas simples e práticas, além de oferecer companhia durante as refeições.",
     },
   ]
@@ -118,7 +123,7 @@ const App: React.FC = () => {
           <Link to='/register' className='row1-link'>Suba agora nessa ponte</Link>
         </div>
         <div className="column image-column">
-          <img src={headerImg} alt="Imagem do cabeçalho" />
+          <img src={Images.headerImg} alt="Imagem do cabeçalho" />
         </div>
       </section>
 
@@ -153,7 +158,7 @@ const App: React.FC = () => {
               key={index}
               title={benefit.title}
               description={benefit.description}
-            // icon={benefit.icon} 
+             icon={benefit.icon} 
             />
           ))}
         </section>
