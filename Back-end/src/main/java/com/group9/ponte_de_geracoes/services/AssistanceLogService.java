678d388eda7a15219edf97f0a48fd59b8276f2ac
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-11 19:12:56 -0300

    #15 - Criado o serviço de AssistanceLogService e implementado o método 'createLog'

---
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/services/AssistanceLogService.java
@@ -0,0 +1,50 @@
+package com.group9.ponte_de_geracoes.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.group9.ponte_de_geracoes.model.AssistanceLog;
+import com.group9.ponte_de_geracoes.model.Assisted;
+import com.group9.ponte_de_geracoes.model.Helper;
+import com.group9.ponte_de_geracoes.repository.AssistanceLogRepository;
+import com.group9.ponte_de_geracoes.repository.AssistedRepository;
+import com.group9.ponte_de_geracoes.repository.HelperRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class AssistanceLogService {
+
+    @Autowired
+    private HelperRepository helperRepository;
+
+    @Autowired
+    private AssistedRepository assistedRepository;
+
+    @Autowired
+    private AssistanceLogRepository assistanceLogRepository;
+
+    public AssistanceLog createLog(AssistanceLog log) {
+        if (log.getHelper() != null && log.getHelper().getId() != null) {
+            Optional<Helper> helper = helperRepository.findById(log.getHelper().getId());
+
+            if (helper.isEmpty()){
+                throw new EntityNotFoundException("Helper not founded");
+            }
+            log.setHelper(helper.get());
+        }
+
+        if (log.getAssisted() != null && log.getAssisted().getId() != null) {
+            Optional<Assisted> assisted = assistedRepository.findById(log.getAssisted().getId());
+            if (assisted.isEmpty()){
+                throw new EntityNotFoundException("Assisted not founded");
+            }
+            log.setAssisted(assisted.get());
+        }
+        return assistanceLogRepository.save(log);
+    }
+}
+
+
