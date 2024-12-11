35a5f05d8b73441ee72fd418837c4b2e25af93ae
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-11 13:02:06 -0300

    #14 - Implementa requisição PUT no controlador de Assisted

--- Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/AssistedController.java
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/controller/AssistedController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,7 +16,9 @@ import com.group9.ponte_de_geracoes.model.Assisted;
 import com.group9.ponte_de_geracoes.repository.AssistedRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -52,6 +55,33 @@ public class AssistedController {
         URI locator = createNewURIById(insertedAssisted);
 
         return ResponseEntity.created(locator).body(insertedAssisted);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Assisted> updateAssisted(@PathVariable Long id, @Validated @RequestBody Assisted requestAssisted) {
+        return assistedRepository.findById(id).map(assistedToUpdate -> {
+            if (requestAssisted.getName() != null) {
+                assistedToUpdate.setName(requestAssisted.getName());
+            }
+            if (requestAssisted.getAge() != null){
+                assistedToUpdate.setAge(requestAssisted.getAge());
+            }
+            if (requestAssisted.getEmail() != null){
+                assistedToUpdate.setEmail(requestAssisted.getEmail());
+            }
+            if (requestAssisted.getPhone() != null){
+                assistedToUpdate.setPhone(requestAssisted.getPhone());
+            }
+            if (requestAssisted.getAddress() != null){
+                assistedToUpdate.setAddress(requestAssisted.getAddress());
+            }
+            if (requestAssisted.getNeeds() != null){
+                assistedToUpdate.setNeeds(requestAssisted.getNeeds());;
+            }
+
+            assistedRepository.save(assistedToUpdate);
+            return ResponseEntity.ok(assistedToUpdate);
+        }).orElse(ResponseEntity.notFound().build());
     }
     
 }
