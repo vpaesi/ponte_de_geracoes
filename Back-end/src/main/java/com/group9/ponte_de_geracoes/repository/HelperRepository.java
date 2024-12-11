e3934715b37716ab8a21e28d6150e7e23587ff17
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-11 16:36:40 -0300

    Correção no HelperRepository que não havia assinatura do método findAll Pageable

--- Back-end/src/main/java/com/group9/ponte_de_geracoes/repository/HelperRepository.java
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/repository/HelperRepository.java
@@ -1,9 +1,13 @@
 package com.group9.ponte_de_geracoes.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import com.group9.ponte_de_geracoes.model.Helper;
 
 public interface HelperRepository extends JpaRepository<Helper, Long> {
-    
+    @NonNull
+    Page<Helper> findAll(@NonNull Pageable pageable);
 }
