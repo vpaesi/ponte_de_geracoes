c3dc70d35e7073bf82138a978df993a7976dcae0
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-13 14:19:04 -0300

    Implementado configurações de CORS dentro do back-end

--- Back-end/src/main/java/com/group9/ponte_de_geracoes/config/WebConfig.java
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.group9.ponte_de_geracoes.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:./uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
