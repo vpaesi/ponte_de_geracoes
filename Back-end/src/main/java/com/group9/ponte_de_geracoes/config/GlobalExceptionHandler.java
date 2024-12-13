a03b0e03bd3dce8a122c5c852e698217a5a1bded
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-13 11:38:58 -0300

    #28 - Criado GlobalExceptionHandler e DTO para respostas customizadas

---
+++ Back-end/src/main/java/com/group9/ponte_de_geracoes/config/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.group9.ponte_de_geracoes.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    
+    
+}
